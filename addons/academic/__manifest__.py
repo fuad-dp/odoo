4b422dccca70807b2e3fd7bc74d632cf403ebe8a
Muhamad Fuad Dwiputra <muhamadfuad@192.168.0.212>
2025-04-15 09:13:42 +0700

    Tugas Odoo - Review 3

---
+++ addons/academic/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name":"Academic Information System v1.0",
+    "version":"1.0",
+    "depends": ["base"],
+    "author":"your name",
+    "category":"Education",
+    "website":"your.website.com",
+    "description":"""long description about your moduels""",
+
+    "data":[
+        "security/groups.xml",
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "course.xml",
+        "session.xml",
+        "attendee.xml",
+        "partner.xml",
+        "wizard/create_attendee.xml",
+    ],
+    "installabe":True,
+    "auto_install":False,
+    "application":True,
+}
